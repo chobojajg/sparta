f09787ea2a1243dc7bb1f74090fc6de6909c8c5d
chobojajg <60211687+chobojajg@users.noreply.github.com>
2024-01-25 15:33:51 +0900

    Add files via upload

---
+++ apikey.js
@@ -0,0 +1,18 @@
+// const fbConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.REACT_APP_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+//     measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// }
+const fbConfig = {
+    apiKey: "AIzaSyD7TRp2xGynRrUsWGF8vzVqZBFVVvZROXA",
+    authDomain: "sparta-b954e.firebaseapp.com",
+    projectId: "sparta-b954e",
+    storageBucket: "sparta-b954e.appspot.com",
+    messagingSenderId: "507121861153",
+    appId: "1:507121861153:web:752b8d7ac0950292fb9960",
+    measurementId: "G-F1KCJSNR1Q"
+}
